f75e36eb33ed3a082b66778ede4fae04bad15cf8
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-01-04 16:53:12 +0100

    style(lint): add format script & formatting files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -15,6 +10,5 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
-  )
+  );
 }
-
