ae5ef8a17c04fc67066e92ab94912ff2381e97d7
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-01-07 12:23:58 +0100

    fix: not adaptive example bg

--- components/Support/Support.tsx
+++ components/Support/Support.tsx
@@ -1,135 +1,19 @@
 const Support = () => {
   return (
-    <section className="support container">
-      {/* <div className="background support__background">
-        <div className="background__figures background__figures_support_scene">
-          <div className="background__primary">
-            <svg
-              width="400"
-              height="411"
-              viewBox="0 0 400 411"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_1"
-            >
-              <path d="M58.6947 96.481C-44.8216 -87.1044 235.592 37.2461 318.354 95.84C406.773 158.439 355.865 308.193 396.789 361.931C437.712 415.669 -102.186 454.028 17.7496 309.755C137.685 165.482 162.211 280.067 58.6947 96.481Z" />
-            </svg>
-            <svg
-              width="699"
-              height="427"
-              viewBox="0 0 699 427"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_2"
-            >
-              <path d="M15.5774 288.416C-77.8779 61.0362 290.833 58.6048 388.142 109.702C496.706 166.71 656.003 -70.5935 689.571 21.7738C723.138 114.141 663.254 611.907 445.598 352.475C227.943 93.0425 109.033 515.795 15.5774 288.416Z" />
-            </svg>
-            <svg
-              width="765"
-              height="393"
-              viewBox="0 0 765 393"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_3"
-            >
-              <path d="M19.0435 268.423C-79.5447 443.082 233.216 390.885 372.858 360.902C591.939 329.976 746.373 126.016 764.022 50.0939C781.671 -25.8278 128.082 -19.8508 344.408 101.691C560.735 223.232 535.439 245.516 497.281 257.585C459.124 269.654 117.632 93.7629 19.0435 268.423Z" />
-            </svg>
-          </div>
-          <div className="background__secondary">
-            <svg
-              width="400"
-              height="411"
-              viewBox="0 0 400 411"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_1"
-            >
-              <path d="M58.6947 96.481C-44.8216 -87.1044 235.592 37.2461 318.354 95.84C406.773 158.439 355.865 308.193 396.789 361.931C437.712 415.669 -102.186 454.028 17.7496 309.755C137.685 165.482 162.211 280.067 58.6947 96.481Z" />
-            </svg>
-            <svg
-              width="699"
-              height="427"
-              viewBox="0 0 699 427"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_2"
-            >
-              <path d="M15.5774 288.416C-77.8779 61.0362 290.833 58.6048 388.142 109.702C496.706 166.71 656.003 -70.5935 689.571 21.7738C723.138 114.141 663.254 611.907 445.598 352.475C227.943 93.0425 109.033 515.795 15.5774 288.416Z" />
-            </svg>
-            <svg
-              width="765"
-              height="393"
-              viewBox="0 0 765 393"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_3"
-            >
-              <path d="M19.0435 268.423C-79.5447 443.082 233.216 390.885 372.858 360.902C591.939 329.976 746.373 126.016 764.022 50.0939C781.671 -25.8278 128.082 -19.8508 344.408 101.691C560.735 223.232 535.439 245.516 497.281 257.585C459.124 269.654 117.632 93.7629 19.0435 268.423Z" />
-            </svg>
-          </div>
-        </div>
-        <div className="background__figures background__figures_support_scene">
-          <div className="background__primary">
-            <svg
-              width="400"
-              height="411"
-              viewBox="0 0 400 411"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_1"
-            >
-              <path d="M58.6947 96.481C-44.8216 -87.1044 235.592 37.2461 318.354 95.84C406.773 158.439 355.865 308.193 396.789 361.931C437.712 415.669 -102.186 454.028 17.7496 309.755C137.685 165.482 162.211 280.067 58.6947 96.481Z" />
-            </svg>
-            <svg
-              width="699"
-              height="427"
-              viewBox="0 0 699 427"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_2"
-            >
-              <path d="M15.5774 288.416C-77.8779 61.0362 290.833 58.6048 388.142 109.702C496.706 166.71 656.003 -70.5935 689.571 21.7738C723.138 114.141 663.254 611.907 445.598 352.475C227.943 93.0425 109.033 515.795 15.5774 288.416Z" />
-            </svg>
-            <svg
-              width="765"
-              height="393"
-              viewBox="0 0 765 393"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_3"
-            >
-              <path d="M19.0435 268.423C-79.5447 443.082 233.216 390.885 372.858 360.902C591.939 329.976 746.373 126.016 764.022 50.0939C781.671 -25.8278 128.082 -19.8508 344.408 101.691C560.735 223.232 535.439 245.516 497.281 257.585C459.124 269.654 117.632 93.7629 19.0435 268.423Z" />
-            </svg>
-          </div>
-          <div className="background__secondary">
-            <svg
-              width="400"
-              height="411"
-              viewBox="0 0 400 411"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_1"
-            >
-              <path d="M58.6947 96.481C-44.8216 -87.1044 235.592 37.2461 318.354 95.84C406.773 158.439 355.865 308.193 396.789 361.931C437.712 415.669 -102.186 454.028 17.7496 309.755C137.685 165.482 162.211 280.067 58.6947 96.481Z" />
-            </svg>
-            <svg
-              width="699"
-              height="427"
-              viewBox="0 0 699 427"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_2"
-            >
-              <path d="M15.5774 288.416C-77.8779 61.0362 290.833 58.6048 388.142 109.702C496.706 166.71 656.003 -70.5935 689.571 21.7738C723.138 114.141 663.254 611.907 445.598 352.475C227.943 93.0425 109.033 515.795 15.5774 288.416Z" />
-            </svg>
-            <svg
-              width="765"
-              height="393"
-              viewBox="0 0 765 393"
-              xmlns="http://www.w3.org/2000/svg"
-              className="background__figure background__figure_3"
-            >
-              <path d="M19.0435 268.423C-79.5447 443.082 233.216 390.885 372.858 360.902C591.939 329.976 746.373 126.016 764.022 50.0939C781.671 -25.8278 128.082 -19.8508 344.408 101.691C560.735 223.232 535.439 245.516 497.281 257.585C459.124 269.654 117.632 93.7629 19.0435 268.423Z" />
-            </svg>
-          </div>
-        </div>
-      </div> */}
-      <h2 className="support__title section-title">Поддержка</h2>
-      <p className="support__description section-body">
-        Мы не оставим Вас без поддержки, сотрудники openmovieapi.dev онлайн
-        24/7. Готовы решить практически любой ваш вопрос, а также - подсказать,
-        помочь, обновить, загрузить, даже доработать код для корректной работы с
-        API kinopoisk.dev.
-      </p>
+    <section className="support">
+      <div className="background support__background">
+        <img src="img/support-left.png" alt="" className="background-left" />
+        <img src="img/support-right.png" alt="" className="background-right" />
+      </div>
+      <div className="container">
+        <h2 className="support__title section-title">Поддержка</h2>
+        <p className="support__description section-body">
+          Мы не оставим Вас без поддержки, сотрудники openmovieapi.dev онлайн
+          24/7. Готовы решить практически любой ваш вопрос, а также -
+          подсказать, помочь, обновить, загрузить, даже доработать код для
+          корректной работы с API kinopoisk.dev.
+        </p>
+      </div>
     </section>
   );
 };
