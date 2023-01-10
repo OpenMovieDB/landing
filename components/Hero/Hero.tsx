01c415f32c1d756eece24510539570b6f8c84271
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-01-10 22:16:27 +0100

    styles(hero): fix adaptive

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,19 +2,21 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <section className="hero container">
-      <h1 className="hero-title">Свободная база фильмов</h1>
-      <p className="section-body hero-description">
-        Получайте максимум данных о фильмах, сериалах, мультфильмах, тв-шоу,
-        аниме, актерах и режиссерах максимально быстро. Наше api содержит данные
-        из таких баз как: kinopoisk, tmbd, imdb
-      </p>
-      <Link href="#" className="hero__link link-gradient">
-        <div className="overlay">
-          <span>Подключить</span>
-          <span>Подключить</span>
-        </div>
-      </Link>
+    <section className="hero">
+      <div className="container">
+        <h1 className="hero-title">Свободная база фильмов</h1>
+        <p className="section-body hero-description">
+          Получайте максимум данных о фильмах, сериалах, мультфильмах, тв-шоу,
+          аниме, актерах и режиссерах максимально быстро. Наше api содержит
+          данные из таких баз как: kinopoisk, tmbd, imdb
+        </p>
+        <Link href="#" className="hero__link link-gradient">
+          <div className="overlay">
+            <span>Подключить</span>
+            <span>Подключить</span>
+          </div>
+        </Link>
+      </div>
     </section>
   );
 };
