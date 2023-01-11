b3d4b437d6957255ef4bdd8245ef98fbfeb350ce
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-01-11 20:12:35 +0100

    chore: add favicons

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,19 @@
 import Link from 'next/link';
+import { useLocomotiveScroll } from 'react-locomotive-scroll';
 
 const Header = () => {
+  const { scroll } = useLocomotiveScroll();
+
+  const handleScroll = (id: string) => {
+    const elem = document.querySelector(id);
+
+    scroll.scrollTo(elem, {
+      offset: '-100',
+      duration: '2000',
+      easing: [0.25, 0.0, 0.35, 1.0],
+    });
+  };
+
   return (
     <header className="header">
       <div className="container">
@@ -9,9 +22,12 @@ const Header = () => {
             <img src="img/logo.svg" alt="Open Movie API" />
           </Link>
           <nav className="header-nav">
-            <Link href="#" className="header-nav__link link-underline">
+            <button
+              onClick={() => handleScroll('#example')}
+              className="header-nav__link link-underline"
+            >
               Пример API
-            </Link>
+            </button>
             <Link href="#" className="header-nav__link link-underline">
               Тарифы
             </Link>
