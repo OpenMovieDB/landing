5306e85828a6a6d9ed2e9a3a944c8af44c63cc34
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-01-21 19:49:44 +0100

    styles: add colors, fonts & font sizes constants

---
+++ styles/theme.ts
@@ -0,0 +1,22 @@
+export const COLORS = {
+  primary: '#0500FF',
+  secondary: '#BD00FF',
+  tertiary: '#00F0FF',
+  light: '#FFFFFF',
+  dark: '#080715',
+  gray: '#D9D9D9',
+};
+
+export const FONTS = {
+  primary: 'ppmi',
+};
+
+export const FONT_SIZES = {
+  sm: '0.8rem',
+  base: '1rem',
+  xl: '1.25rem',
+  '2xl': '1.563rem',
+  '3xl': '2rem',
+  '4xl': '3.555rem',
+  '5xl': '5.333rem',
+};
