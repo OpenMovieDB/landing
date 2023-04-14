a7e0d6da6749909d2e132304e31f8d0590a910fe
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-04-15 00:15:33 +0200

    refactor: move is mobile to hook

--- hooks/useMobileDevice.tsx
+++ hooks/useMobileDevice.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from 'react';
 
-import { isMobileDevice } from '../utils/detectMobileDevice';
-
 export const useMobileDevice = (): boolean => {
+  const isMobileDevice = (): boolean => {
+    if (typeof window === 'undefined' || !window.navigator.userAgent) {
+      return false;
+    }
+
+    const { userAgent } = window.navigator;
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+  };
+
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
--- utils/detectMobileDevice.ts
+++
@@ -1,8 +0,0 @@
-export const isMobileDevice = (): boolean => {
-  if (typeof window === 'undefined' || !window.navigator.userAgent) {
-    return false;
-  }
-
-  const { userAgent } = window.navigator;
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-};
