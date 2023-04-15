984845d00e4ef8c63c335af8eb345c9db8dcd2dd
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-04-16 01:50:24 +0200

    ✨ feat(ui): add SectionContainer component to handle section styling

---
+++ components/ui/SectionContainer.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SectionContainer = styled.section`
+  @media (max-width: 1749px) {
+    padding: 0 15px 0 15px;
+  }
+`;
+
+export default SectionContainer;
