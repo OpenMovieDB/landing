5044a201b9b9eb6a6bbbeba5c621d5666ba4c203
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-04-16 01:49:51 +0200

    🔨 refactor(ui): extract parallax scrolling logic into a custom hook ✨ feat(ui): add useParallaxScrolling hook to enable parallax scrolling on components

--- components/ui/ParallaxSlider.tsx
+++ components/ui/ParallaxSlider.tsx
@@ -1,15 +1,6 @@
-import {
-  motion,
-  useAnimationFrame,
-  useMotionValue,
-  useScroll,
-  useSpring,
-  useTransform,
-  useVelocity,
-  wrap,
-} from 'framer-motion';
-import { useRef } from 'react';
+import { motion, useTransform, wrap } from 'framer-motion';
 import styled from 'styled-components';
+import { useParallaxScrolling } from '../../hooks/useParallaxScrolling';
 
 interface ParallaxProps {
   children: any;
@@ -30,33 +21,9 @@ const ScrollerRow = styled(motion.div)`
 `;
 
 const ParallaxSlider = ({ children, baseVelocity = 10 }: ParallaxProps) => {
-  const baseX = useMotionValue(0);
-  const { scrollY } = useScroll();
-  const scrollVelocity = useVelocity(scrollY);
-  const smoothVelocity = useSpring(scrollVelocity, {
-    damping: 50,
-    stiffness: 400,
-  });
-  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-    clamp: false,
-  });
+  const { baseX } = useParallaxScrolling(baseVelocity);
 
   const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
-
-  const directionFactor = useRef<number>(1);
-  useAnimationFrame((t, delta) => {
-    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
-
-    if (velocityFactor.get() < 0) {
-      directionFactor.current = -1;
-    } else if (velocityFactor.get() > 0) {
-      directionFactor.current = 1;
-    }
-
-    moveBy += directionFactor.current * moveBy * velocityFactor.get();
-
-    baseX.set(baseX.get() + moveBy);
-  });
 
   return (
     <Row>
