960a526e80a847a79f9a6fec6de1fcc96ec08504
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-04-15 16:51:55 +0200

    fix(sources):  images outside the container break th layout

--- components/Cources/Sources.tsx
+++ components/Cources/Sources.tsx
@@ -1,81 +1,81 @@
-import React, { useEffect, useState } from 'react';
+import React, { FC, memo, useState, useEffect } from 'react';
 import styled from 'styled-components';
+import useWindowSize from '../../hooks/useWindowSize';
 
-const SourcesContainer = styled.div`
+interface Source {
+  name: string;
+  logo: string;
+}
+
+interface SourcesProps {
+  sources: Source[];
+}
+
+const SourcesContainer = styled.div<{ size: number }>`
   position: absolute;
   width: 100%;
   height: 100%;
-  max-width: 600px;
-  max-height: 600px;
-
-  @media screen and (max-width: 768px) {
-    max-width: 400px;
-    max-height: 400px;
-  }
-
-  @media screen and (max-width: 500px) {
-    max-width: 300px;
-    max-height: 300px;
-  }
+  max-width: ${(props) => props.size}px;
+  max-height: ${(props) => props.size}px;
 `;
 
-const SourceImage = styled.img`
+const SourceImage = styled.img<{ size: number }>`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-
-  width: 65px;
-  height: 65px;
-
-  @media screen and (max-width: 768px) {
-    width: 50px;
-    height: 50px;
-  }
-
-  @media screen and (max-width: 500px) {
-    width: 31.79px;
-    height: 31.79px;
-  }
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
 `;
 
-const Sources = ({ sources }: { sources: { name: string; logo: string }[] }) => {
-  const [dimensions, setDimensions] = useState({ width: 600, height: 600 });
 
-  useEffect(() => {
-    const updateDimensions = () => {
-      const width = window.innerWidth;
+const Sources: FC<SourcesProps> = memo(({ sources }) => {
+  const windowWidth = useWindowSize().width;
 
-      if (width > 768) {
-        setDimensions({ width: 600, height: 600 });
-      } else if (width > 500) {
-        setDimensions({ width: 400, height: 400 });
-      } else {
-        setDimensions({ width: 300, height: 300 });
-      }
-    };
+  const calculateDimensions = (windowWidth: number): { width: number; height: number } => {
+    if (windowWidth > 768) {
+      return { width: 600, height: 600 };
+    } else if (windowWidth > 500) {
+      return { width: 400, height: 400 };
+    } else {
+      return { width: 300, height: 300 };
+    }
+  };
 
-    updateDimensions();
-    window.addEventListener('resize', updateDimensions);
-    return () => window.removeEventListener('resize', updateDimensions);
-  }, []);
+  const calculateCircularPosition = (
+    index: number,
+    sourcesCount: number,
+    dimensions: { width: number; height: number }
+  ): { x: number; y: number } => {
+    const radius = dimensions.width * 0.43;
+    const centerX = dimensions.width * 0.5;
+    const centerY = dimensions.height * 0.5;
+    const angleStep = 360 / sourcesCount;
+    const angle = index * angleStep;
 
-  const radius = dimensions.width * 0.43;
-  const centerX = dimensions.width * 0.5;
-  const centerY = dimensions.height * 0.5;
+    const x = Math.round(centerX + radius * Math.cos((angle * Math.PI) / 180));
+    const y = Math.round(centerY + radius * Math.sin((angle * Math.PI) / 180));
+  
+    return { x, y };
+  };
+
+  
+  const dimensions = calculateDimensions(windowWidth)
+
+  const imageSize = dimensions.width / 9;
   const sourcesCount = sources.length;
-  const angleStep = 360 / sourcesCount;
 
   return (
-    <SourcesContainer>
+    <SourcesContainer size={dimensions.width}>
       {sources.map((source, index) => {
-        const angle = index * angleStep;
-        const x = Math.round(centerX + radius * Math.cos((angle * Math.PI) / 180));
-        const y = Math.round(centerY + radius * Math.sin((angle * Math.PI) / 180));
-        return <SourceImage key={index} src={source.logo} alt={source.name} style={{ top: y, left: x }} />;
+        const { x, y } = calculateCircularPosition(index, sourcesCount, dimensions);
+        return (
+          <SourceImage key={index} src={source.logo} alt={source.name} size={imageSize} style={{ top: y, left: x }} />
+        );
       })}
     </SourcesContainer>
   );
-};
+});
 
 export default Sources;
+
