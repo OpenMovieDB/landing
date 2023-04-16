cbe8e76f12ccfa547150354353ee2431c6d7e268
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-04-16 14:51:14 +0200

    ✨ feat(AnimatedCounter.tsx): add AnimatedCounter component with props from and to to animate a counter from a number to another.

---
+++ components/ui/AnimatedCounter.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import { motion, useMotionValue, useTransform, useInView, animate } from 'framer-motion';
+
+type AnimatedCounterProps = {
+  from: number;
+  to: number;
+};
+
+const AnimatedCounter = ({ from, to }: AnimatedCounterProps) => {
+  const count = useMotionValue(from);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
+  const ref = useRef(null);
+  const inView = useInView(ref);
+
+  useEffect(() => {
+    if (inView) {
+      animate(count, to, { duration: 2 });
+    }
+  }, [count, inView, to]);
+
+  return <motion.span ref={ref}>{rounded}</motion.span>;
+};
+
+export default AnimatedCounter;
