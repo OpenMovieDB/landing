34a720b6c04a58a1fe0b064c8d30641c2cba4496
Evgenii Fedoryachenko <mdwit0r@gmail.com>
2023-05-01 22:26:00 +0200

    style: change background and theme color in site.webmanifest refactor: remove gsap from next-transpile-modules in next.config.js chore: update dependencies in package.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require('next-transpile-modules')(['gsap']);
 
-module.exports = withTM({
+module.exports = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-});
+}
